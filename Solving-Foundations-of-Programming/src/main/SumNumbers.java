191284bb5e1e7d3c1949238b57fc6e0fad7b37fd
cute3954 <cute3954@naver.com>
2020-05-04 14:32:15 +0900

    5/4 Problem Solving with Java_SumNumbers

---
+++ Solving-Foundations-of-Programming/src/main/SumNumbers.java
@@ -0,0 +1,21 @@
+package main;
+
+/* https://codingbat.com/prob/p121193
+ * Given a string, return the sum of the numbers appearing in the string, ignoring all other characters. A number is a series of 1 or more digit chars in a row. (Note: Character.isDigit(char) tests if a char is one of the chars '0', '1', .. '9'. Integer.parseInt(string) converts a string to an int.)
+ * 
+ * sumNumbers("abc123xyz") → 123
+ * sumNumbers("aa11b33") → 44
+ * sumNumbers("7 11") → 18
+ * */
+	
+public class SumNumbers {
+	public static int sumNum(String str) {
+		int total = 0;
+		for (String numString : str.split("[^0-9]+")) {
+			if (!numString.isEmpty()) {
+				total += Integer.parseInt(numString);
+			}
+		}
+		return total;
+	}
+}
---
+++ Solving-Foundations-of-Programming/src/main/SumNumbers_main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class SumNumbers_main {
+
+	public static void main(String[] args) {
+		SumNumbers sumNum = new SumNumbers();
+		int sum = SumNumbers.sumNum("aa11b33");
+		System.out.println();
+	}
+
+}
